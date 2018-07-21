218263f83213251772b14b6c0948f1b01cb7a503
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2018-07-21 14:06:16 -0400

    action directory and intial file setup

---
+++ src/actions/boycottActions.js
@@ -0,0 +1,8 @@
+// export const POKEMON_ACTIONS = {
+//     SET_IS_LOADING: 'pokemon/SET_IS_LOADING',
+//     SET_POKEMON: 'pokemon/SET_POKEMON',
+//   };
+
+export const BOYCOTT_ACTIONS ={
+    
+}
