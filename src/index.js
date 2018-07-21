ba0c702055ed9b0c44a17a9a80c76754a7dc7879
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2018-07-21 14:09:14 -0400

    inital redux setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from "react";
+import {Provider} from 'react-redux';
 import ReactDOM from "react-dom";
+import configureStore from './store/configureStore';
 import "./index.css";
-import App from "./App";
+import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
 
 import { healthCheck } from "./api/health";
 
+const store = configureStore();
+
 ReactDOM.render(
-  <App healthCheck={healthCheck} />,
+  <Provider store={store}>
+      <App />
+  </Provider>,
   document.getElementById("root")
 );
 registerServiceWorker();
