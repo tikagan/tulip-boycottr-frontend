514616a331ffc4e1e43c6e1242bc84c650fb9e78
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2018-07-22 13:16:48 -0400

    somehow MapContainer got added, removed to pass unit test

--- src/components/App.js
+++ src/components/App.js
@@ -16,13 +16,6 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <div className="App-intro">
-          <div className="map-container">
-              <MapContainer
-                className="map" 
-              />
-          </div>
-        </div>
       </div>
     );
   }
