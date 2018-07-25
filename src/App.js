07f316a97360dd233f702a3103e14dd3cab77786
Meaghan Eleanor <meaghan.eleanor@gmail.com>
2018-07-24 20:55:43 -0400

    initial map component structure is there, api key seems to not work (#5)
    * initial map component structure is there, api key seems to not work
    * added props constructor
    * google-maps-react package added to dependencies
    * added .DS_Store
    * removed unused constructor
    * added EOF line to MapContainer (#6)
    * added EOF line to MapContainer
    * removes constructor in MapContainer, comments out healthCheck to pass CI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+
+import MapContainer from "./components/MapContainer";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -6,7 +8,7 @@ class App extends Component {
   componentDidMount() {
     // this is here as an example for how to connect to the backend
     // it should be removed once development has started
-    this.props.healthCheck();
+    // this.props.healthCheck();
   }
 
   render() {
@@ -16,9 +18,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <div className="map-container">
+              <MapContainer
+                className="map" 
+              />
+          </div>
+        </div>
       </div>
     );
   }
